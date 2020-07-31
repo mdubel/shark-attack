27ee531050b6eff30c15b50cae5b4d90a1f494d8
Kamil Zyla <kamil@appsilon.com>
2020-07-31 07:45:01 +0200

    Remove unused Shiny tasks.py

--- src/tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke import task, Collection
-
-ns = Collection('shiny')
-
-@task
-def init(c):
-    """Install dependencies for this pattern"""
-    c.run("echo 'hello world'")
-ns.add_task(init)
