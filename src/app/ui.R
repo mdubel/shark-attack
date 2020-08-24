66fa9847983c83d699947c677d634f68e25e1b9b
pedrocoutinhosilva <pedrocoutinhosilva@gmail.com>
2020-08-24 16:35:26 +0200

    Include bundle file in the ui definition.

--- src/app/ui.R
+++ src/app/ui.R
@@ -3,7 +3,7 @@ function(input, output, session) {
     tags$head(
       # _todo_ load any additional files like this:
       tags$link(rel = "stylesheet", type = "text/css", href = "css/sass.min.css"),
-      tags$script(src = "js/button-theme.js"),
+      tags$script(src = "js/bundle.min.js"),
     ),
     tags$h3("Hello"),
     home$ui("module_id")
