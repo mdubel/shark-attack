8b8ab0a77fc2c26d9235b30f594dde6a9bf8c506
Marcin Dubel <marcin@appsilon.com>
2021-04-24 16:41:42 +0200

    Add text paragraph to bite modal

--- src/app/modules/gameOver.R
+++ src/app/modules/gameOver.R
@@ -22,9 +22,19 @@ server <- function(input, output, session, ObjectsManager) {
         className = "bite-modal",
         isOpen = session$userData$isBiteModalOpen(), isBlocking = FALSE,
         div(
-          style = "margin: 20px",
-          ShinyComponentWrapper(DefaultButton(session$ns("playAgain"), text = "Play Again!")),
-          ShinyComponentWrapper(DefaultButton(session$ns("hideModal"), text = "Close"))
+          class = "modal-grid",
+          div(
+            Text(variant = "xLarge", "The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!"),
+            class = "modal-element modal-element--text"
+          ),
+          div(
+            ShinyComponentWrapper(PrimaryButton(session$ns("playAgain"), text = "Play Again!")),
+            class = "modal-element modal-element--play"
+          ),
+          div(
+            ShinyComponentWrapper(DefaultButton(session$ns("hideModal"), text = "Close")),
+            class = "modal-element modal-element--learn"
+          )
         )
       )
     )
