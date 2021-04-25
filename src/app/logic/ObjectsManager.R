aadce65613132165cca21235e5afe48a5980f85d
Marcin Dubel <marcin@appsilon.com>
2021-04-25 18:48:49 +0200

    Control multiple objects and restructure objects in class (#8)

--- src/app/logic/ObjectsManager.R
+++ src/app/logic/ObjectsManager.R
@@ -13,12 +13,14 @@ ObjectsManager <- R6::R6Class(
   "ObjectsManager",
   inherit = GridManager,
   private = list(
-    diver = NULL,
-    shark = NULL,
-    chest = NULL,
-    key = NULL,
-    boat = NULL,
-    plants = NULL,
+    objects = list(
+      diver = c(),
+      shark = c(),
+      chest = c(),
+      key = c(),
+      boat = c(),
+      plants = c()
+    ),
     
     is_key = FALSE,
     is_chest = FALSE,
@@ -35,7 +37,7 @@ ObjectsManager <- R6::R6Class(
     
     can_object_move = function(location, direction) {
       new_location <- private$get_new_location(location, direction)
-      if(any(new_location == 0) || new_location[1] > private$number_of_columns || new_location[2] > private$number_of_rows) {
+      if(isTRUE(any(new_location == 0) || new_location[1] > private$number_of_columns || new_location[2] > private$number_of_rows)) {
         return(FALSE)
       } else {
         return(TRUE)
@@ -44,17 +46,15 @@ ObjectsManager <- R6::R6Class(
     
     can_object_pass = function(object_name, location, direction) {
       new_location <- private$get_new_location(location, direction)
-      key_location <- private$get_grid_element_location(private$key)
-      chest_location <- private$get_grid_element_location(private$chest)
-      
+      new_location_id <- private$prepare_grid_element_id(new_location[1], new_location[2]) 
       if(object_name == "shark") {
-        if(all(new_location == key_location) || all(new_location == chest_location)) {
+        if(all(new_location_id == private$objects$key) || all(new_location_id == private$objects$chest) || any(new_location_id == private$objects$plants)) {
           return(FALSE)
         } else {
           return(TRUE)
         }
       } else if(object_name == "diver") {
-        if(all(new_location == chest_location) && !private$is_key) {
+        if((all(new_location_id == private$objects$chest) && !private$is_key) || any(new_location_id == private$objects$plants)) {
           return(FALSE)
         } else {
           return(TRUE)
@@ -64,33 +64,79 @@ ObjectsManager <- R6::R6Class(
     
     get_new_location = function(location, direction) {
       private$get_grid_element_location(location) + private$get_move_vector(direction)
+    },
+    
+    occupied_grids = function() {
+      private$objects %>% unlist() %>% unname()
     }
   ),
   public = list(
     add_on_grid = function(object_name, location) {
-      private[[object_name]] <- location
+      self$clean_grid(location)
+      private$objects[[object_name]] <- location
+      shinyjs::runjs(glue("$('#{location}').css('background-image', 'url(./assets/{object_name}.png)');"))
+    },
+    
+    add_on_multiple_grid = function(object_name, location) {
+      self$clean_grid(location)
+      private$objects[[object_name]] <- c(private$objects[[object_name]], location)
       shinyjs::runjs(glue("$('#{location}').css('background-image', 'url(./assets/{object_name}.png)');"))
     },
     
     place_objects = function() {
-      if(!is.null(private$diver)) {
-        self$clean_grid(private$diver)
-        self$clean_grid(private$chest)
-        self$clean_grid(private$key)
-      }
+      self$clean_it_all()
 
-      self$add_on_grid("diver", private$prepare_grid_element_id(1, 1))
-      self$add_on_grid("chest", private$random_grid_location(c(private$number_of_rows, private$number_of_rows), 1:private$number_of_columns))
-      self$add_on_grid("key", private$random_grid_location(2:(private$number_of_rows - 1) , 1:private$number_of_columns))
-      self$add_on_grid("shark", private$random_grid_location(2:private$number_of_rows, 2:private$number_of_columns))
+      self$add_on_grid(
+        "diver",
+        private$prepare_grid_element_id(1, 1)
+      )
+      self$add_on_grid(
+        "chest",
+        private$random_grid_location(
+          c(private$number_of_rows, private$number_of_rows),
+          1:private$number_of_columns,
+          private$occupied_grids()
+        )
+      )
+      self$add_on_grid(
+        "key",
+        private$random_grid_location(
+          2:(private$number_of_rows - 1),
+          1:private$number_of_columns,
+          private$occupied_grids()
+        )
+      )
+      self$add_on_grid(
+        "shark",
+        private$random_grid_location(
+          2:private$number_of_rows,
+          2:private$number_of_columns,
+          private$occupied_grids()
+        )
+      )
+      
+      purrr::walk(
+        seq_len(private$number_of_plants),
+        function(x) {
+          self$add_on_multiple_grid(
+            "plants",
+            private$random_grid_location(
+              1:private$number_of_rows,
+              1:private$number_of_columns,
+              private$occupied_grids()
+            )
+          )
+        }
+      )
+      
       shinyjs::runjs("randomMove('shark');")
     },
     
     check_shark_bite = function() {
-      if(private$diver == private$shark) {
+      if(private$objects$diver == private$objects$shark) {
         shinyjs::runjs("stopMove();")
-        self$clean_grid(private$diver)
-        self$add_on_grid("shark", private$shark)
+        self$clean_grid(private$objects$diver)
+        self$add_on_grid("shark", private$objects$shark)
         return(TRUE)
       } else {
         return(FALSE)
@@ -98,16 +144,16 @@ ObjectsManager <- R6::R6Class(
     },
     
     check_collect = function() {
-      if(private$diver == private$key) {
+      if(private$objects$key == private$objects$diver) {
         private$is_key <- TRUE
       }
-      if(private$diver == private$chest) {
+      if(private$objects$chest == private$objects$diver) {
         private$is_chest <- TRUE
       }
     },
     
     check_success = function() {
-      if(private$is_chest && private$diver == private$prepare_grid_element_id(1, 1)) {
+      if(private$is_chest && isTRUE(private$objects$diver == private$prepare_grid_element_id(1, 1))) {
         shinyjs::runjs("stopMove();")
         return(TRUE)
       } else {
@@ -116,7 +162,7 @@ ObjectsManager <- R6::R6Class(
     },
     
     move_object = function(object_name, direction) {
-      location <- private[[object_name]]
+      location <- private$objects[[object_name]]
       if(private$can_object_move(location, direction) && private$can_object_pass(object_name, location, direction)) {
         self$clean_grid(location)
         
@@ -125,6 +171,20 @@ ObjectsManager <- R6::R6Class(
         
         self$add_on_grid(object_name, new_location_id)
       }
+    },
+
+    clean_grid = function(locations) {
+      purrr::walk(
+        locations, 
+        function(location) shinyjs::runjs(glue("$('#{location}').css('background-image', 'none');"))
+      )
+    },
+    
+    clean_it_all = function() {
+      shinyjs::runjs("$('.single-grid').css('background-image', 'none');")
+      private$is_key <- FALSE
+      private$is_chest <- FALSE
+      purrr::walk(names(private$objects), function(object_name) private$objects[[object_name]] <- c())
     },
     
     initialize = function() {
