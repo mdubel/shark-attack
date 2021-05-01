19cfbc9a38290f2a1aac5299d2274b353b69f5a9
Marcin Dubel <marcin@appsilon.com>
2021-05-01 13:26:32 +0200

    Add all trash types

--- src/app/logic/ObjectsManager.R
+++ src/app/logic/ObjectsManager.R
@@ -35,18 +35,31 @@ ObjectsManager <- R6::R6Class(
     number_of_plants = 10, 
     number_of_sharks = 8,
     initial_trash_count = 10,
+    new_trash_count = 1,
     
     trash = list(
-      one = c(),
-      two = c()
+      organic = c(),
+      glass = c(),
+      metal = c(),
+      electro = c(),
+      plastic = c(),
+      radio = c()
     ),
     trash_chances = list(
-      one = 0.2,
-      two = 0.8
+      organic = 0.05,
+      glass = 0.15,
+      metal = 0.15,
+      electro = 0.2,
+      plastic = 0.4,
+      radio = 0.05
     ),
     trash_points = list(
-      one = 1,
-      two = 2
+      organic = 1,
+      glass = 3,
+      metal = 3,
+      electro = 7,
+      plastic = 5,
+      radio = 15
     ),
     
     get_move_vector = function(direction) {
@@ -59,8 +72,7 @@ ObjectsManager <- R6::R6Class(
       )
     },
     
-    can_object_move = function(location, direction) {
-      new_location <- private$get_new_location(location, direction)
+    can_object_move = function(new_location) {
       if(isTRUE(any(new_location == 0) || new_location[1] > private$number_of_columns || new_location[2] > private$number_of_rows)) {
         return(FALSE)
       } else {
@@ -68,8 +80,7 @@ ObjectsManager <- R6::R6Class(
       }
     },
     
-    can_object_pass = function(object_name, location, direction) {
-      new_location <- private$get_new_location(location, direction)
+    can_object_pass = function(object_name, new_location) {
       new_location_id <- private$prepare_grid_element_id(new_location[1], new_location[2]) 
       if(object_name == "shark") {
         if(new_location_id %in% private$objects$plants || new_location_id == private$objects$boat || new_location_id %in% private$objects$shark || new_location_id %in% private$occupied_trash()) {
@@ -86,8 +97,7 @@ ObjectsManager <- R6::R6Class(
       }
     },
     
-    can_trash_pass = function(location, direction) {
-      new_location <- private$get_new_location(location, direction)
+    can_trash_pass = function(new_location) {
       new_location_id <- private$prepare_grid_element_id(new_location[1], new_location[2])
       if(new_location_id %in% c(private$occupied_grids(), private$occupied_trash())) {
         return(FALSE)
@@ -204,10 +214,10 @@ ObjectsManager <- R6::R6Class(
     
     move_object = function(object_name, direction, index = 1) {
       location <- private$objects[[object_name]][index]
-      if(private$can_object_move(location, direction) && private$can_object_pass(object_name, location, direction)) {
+      new_location <- private$get_new_location(location, direction)
+      if(private$can_object_move(new_location) && private$can_object_pass(object_name, new_location)) {
         private$clean_grid(location)
         
-        new_location <- private$get_new_location(location, direction)
         new_location_id <- private$prepare_grid_element_id(new_location[1], new_location[2])
         
         self$add_on_grid(
@@ -227,16 +237,16 @@ ObjectsManager <- R6::R6Class(
       private$set_scores()
     },
     
-    place_trash = function() {
+    place_trash = function(count = private$initial_trash_count, col_range = 1:private$number_of_columns, row_range = 1:private$number_of_rows) {
       purrr::walk(
-        seq_len(private$initial_trash_count),
+        seq_len(count),
         function(index) {
           trash_name <- private$random_trash()
           self$add_on_grid(
             trash_name,
             private$random_grid_location(
-              1:private$number_of_columns,
-              1:private$number_of_rows,
+              col_range,
+              row_range,
               c(private$occupied_grids(), private$occupied_trash())
             ),
             index = length(private$trash[[trash_name]]) + 1,
@@ -265,6 +275,8 @@ ObjectsManager <- R6::R6Class(
           iwalk(private$trash[[trash_name]], ~self$move_trash(trash_name, .x, sample(c("left", "down"), 1), .y))
         }
       )
+      self$place_trash(private$new_trash_count, c(private$number_of_columns, private$number_of_columns), 1:private$number_of_rows)
+      self$place_trash(private$new_trash_count, 1:private$number_of_columns, c(1, 1))
     },
     
     remove_trash = function(trash_name, location) {
@@ -272,10 +284,10 @@ ObjectsManager <- R6::R6Class(
     },
     
     move_trash = function(trash_name, location, direction, index = 1) {
-      if(private$can_object_move(location, direction)) {
-        if(private$can_trash_pass(location, direction)) {
+      new_location <- private$get_new_location(location, direction)
+      if(private$can_object_move(new_location)) {
+        if(private$can_trash_pass(new_location)) {
           private$clean_grid(location)
-          new_location <- private$get_new_location(location, direction)
           new_location_id <- private$prepare_grid_element_id(new_location[1], new_location[2])
           
           self$add_on_grid(
