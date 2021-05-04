f67d33ea21cbe2e8cf1e09e3002f7d61e9c7f6a6
Marcin Dubel <marcin@appsilon.com>
2021-05-04 09:46:00 +0200

    Add a favicon

--- src/app/ui.R
+++ src/app/ui.R
@@ -1,5 +1,6 @@
 fluentPage(
   tags$head(
+    tags$link(rel = "icon", type = "image/png", href = "assets/shark.png"),
     tags$link(rel = "stylesheet", type = "text/css", href = "css/sass.min.css"),
     tags$script(src = "js/key-capture.js"),
     tags$script(src = "js/select-difficulty.js"),
