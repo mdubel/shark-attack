18740f9c01e5fd08c4ae5011d0bb59bf8e3b5929
Marcin Dubel <marcin@appsilon.com>
2021-05-07 09:43:39 +0200

    Game tutorial (#18)
    * Organize the tutorial modal with steps
    
    * Add carousel mechanism to tutorial
    
    * Add mechanism to modify the tutorial image
    
    * First version of tutorial images
    
    * Redundant files cleaning
    
    * Deployment connection
    
    * Added tutorial texts

--- src/app/modules/gameStart.R
+++ src/app/modules/gameStart.R
@@ -7,9 +7,7 @@ export("ui", "init_server")
 
 ui <- function(id) {
   ns <- NS(id)
-  tagList(
-    reactOutput(ns("startModal"))
-  )
+  reactOutput(ns("startModal"))
 }
 
 init_server <- function(id, ObjectsManager, consts) {
@@ -20,33 +18,57 @@ server <- function(input, output, session, ObjectsManager, consts) {
   ns <- session$ns
   
   output$startModal <- renderReact({
+    shinyjs::runjs("setLevelClick();")
     reactWidget(
       Modal(
         className = "modal",
         isOpen = session$userData$isStartModalOpen(), isBlocking = FALSE,
         div(
           class = "start-grid",
-          start_content()
+          div(
+            div(ShinyComponentWrapper(IconButton(ns("step_left"), iconProps = list(iconName = "ChevronLeft"))), class = "arrow arrow--left"),
+            div(ShinyComponentWrapper(SwatchColorPicker(ns("tutorial_step"), "1", colorCells = tutorial_steps_config, columnCount = length(tutorial_steps_config))), class = "steps"),
+            div(class = "image", img(src = "./assets/tutorial1.png")),
+            div(class = "text", h4(consts$tutorial[[1]])),
+            div(ShinyComponentWrapper(IconButton(ns("step_right"), iconProps = list(iconName = "ChevronRight"))), class = "arrow arrow--right"),
+            class = "start-element start-element--tutorial"
+          ),
+          span(
+            class = "start-element--levels",
+            div(class = "level-icon--text", h4("Select difficulty")),
+            div(class = "level-icon--easy", level_icon("easy")),
+            div(class = "level-icon--medium", level_icon("medium")),
+            div(class = "level-icon--hard", level_icon("hard"))
+          )
         )
       )
     )
   })
   
   level_icon <- function(type) {
-    div(img(src = glue("./assets/{type}.png")), class = "start-element start-element--icon", id = glue("level-{type}"))
+    div(img(src = glue("./assets/{type}.png")), class = "start-element start-element--icon", id = glue("level-{type}"))#, onClick = JS(glue("() => setLevel('{type}');")))
   }
   
-  start_content <- function() {
-    shinyjs::runjs("setLevelClick();")
-    tagList(
-      div(
-        # TODO Implement carousel with game tutorial
-        class = "start-element start-element--carousel"
-      ),
-      div(class = "start-element start-element--text", h4("Select difficulty")),
-      div(class = "level-icon--easy", level_icon("easy")),
-      div(class = "level-icon--medium", level_icon("medium")),
-      div(class = "level-icon--hard", level_icon("hard"))
-    )
-  }
+  tutorial_steps_config <- lapply(seq_along(consts$tutorial), function(index) {
+    list(id = as.character(index), color = consts$colors$success)
+  })
+  
+  observeEvent(input$tutorial_step, {
+    text <- consts$tutorial[[input$tutorial_step]]
+    shinyjs::runjs(glue("updateTutorial('{input$tutorial_step}', '{text}');"))
+  })
+  
+  observeEvent(input$step_left, {
+    step <- as.numeric(input$tutorial_step)
+    if(step > 1) {
+      updateSwatchColorPicker(session, "tutorial_step", as.character(step - 1))
+    }
+  })
+  
+  observeEvent(input$step_right, {
+    step <- as.numeric(input$tutorial_step)
+    if(step < length(consts$tutorial)) {
+      updateSwatchColorPicker(session, "tutorial_step", as.character(step + 1))
+    }
+  })
 }
--- src/app/ui.R
+++ src/app/ui.R
@@ -4,11 +4,12 @@ fluentPage(
     tags$link(rel = "stylesheet", type = "text/css", href = "css/sass.min.css"),
     tags$script(src = "js/key-capture.js"),
     tags$script(src = "js/select-difficulty.js"),
-    tags$script(src = "js/timer.js")
+    tags$script(src = "js/timer.js"),
+    tags$script(src = "js/tutorial.js")
   ),
   useShinyjs(),
   withReact(
-    Persona(className = "score_board", imageInitials = "0", primaryText = "1:00", initialsColor = "#41aa00"),
+    Persona(className = "score_board", imageInitials = "0", primaryText = "1:00", initialsColor = consts$colors$success),
     gameStart$ui("gameStart"),
     uiOutput("grid"),
     gameOver$ui("gameOver")
